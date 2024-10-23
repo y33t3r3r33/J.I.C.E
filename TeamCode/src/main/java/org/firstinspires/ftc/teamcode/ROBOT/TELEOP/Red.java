e6b598ee4f733802e78ab3462929b272741beb9b
y33t3r3r33 <dumasdd9@gmail.com>
2024-10-23 02:43:48 -0500

    Pain

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ROBOT/TELEOP/Red.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ROBOT/TELEOP/Red.java
@@ -1,25 +1,45 @@
 package org.firstinspires.ftc.teamcode.ROBOT.TELEOP;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.ROBOT.Hardware;
 
 @TeleOp(name="RedTeleOp")
-public class Red extends OpMode {
+public class Red extends LinearOpMode {
    private Hardware robot = Hardware.instance;
 
 
-
     @Override
-    public void init() {
+    public void runOpMode() {
         robot = robot.getInstance();
         robot.init(hardwareMap);
 
-    }
 
-    @Override
-    public void loop() {
+        // Stop and reset encoders
+        //leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //wrist.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
+        // Set motors to use encoders
+        //leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        //rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // Set motor direction
+        //leftMotor.setDirection(DcMotor.Direction.REVERSE);
+        //rightMotor.setDirection(DcMotor.Direction.FORWARD);
+        //Set zero power behavior
+        //Forearm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //Arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //leftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        //rightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // Run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
     }
 }
