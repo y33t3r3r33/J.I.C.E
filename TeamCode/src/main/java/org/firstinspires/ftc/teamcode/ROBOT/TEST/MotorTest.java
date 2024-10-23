924284f536924d996c58cf36912bd92519cd4fb8
y33t3r3r33 <dumasdd9@gmail.com>
2024-10-23 13:29:11 -0500

    Motor test created

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ROBOT/TEST/MotorTest.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.ROBOT.TEST;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.ROBOT.Hardware;
+
+@TeleOp(name = "MotorTest", group = "Test")
+public class MotorTest extends LinearOpMode {
+
+    @Override
+    public void runOpMode(){
+
+        double testPower;
+        double testMove = (gamepad1.left_stick_y);
+
+    }
+}
