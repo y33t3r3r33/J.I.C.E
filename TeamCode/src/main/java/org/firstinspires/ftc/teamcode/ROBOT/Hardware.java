99fcdc3b84086f01efee6cf8f3ab016046599877
y33t3r3r33 <dumasdd9@gmail.com>
2024-10-24 13:53:06 -0500

    Fixed some stuff in the hardware file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ROBOT/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ROBOT/Hardware.java
@@ -1,21 +1,23 @@
 package org.firstinspires.ftc.teamcode.ROBOT;
 
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
 
 public class Hardware {
 
     public static Hardware instance = null;
     private HardwareMap hmap;
 
-    public DcMotor leftMotor;
-    public DcMotor rightMotor;
-    public DcMotor Forearm;
-    public DcMotor Arm;
+    public MotorEx FRMotor;
+    public MotorEx FLMotor;
+    public MotorEx BRMotor;
+    public MotorEx BLMotor;
 
-    public Servo Claw;
+    public MotorEx Extend;
+
     public CRServo Intake;
 
 
@@ -37,13 +39,17 @@ public class Hardware {
     public void init(HardwareMap hmap){
         this.hmap = hmap;
 
-        leftMotor = hmap.get(DcMotor.class, "leftmotor");
-        rightMotor = hmap.get(DcMotor.class, "rightmotor");
-        Forearm = hmap.get(DcMotor.class,"forearm");
-        Arm = hmap.get(DcMotor.class,"arm");
-        Claw = hmap.get(Servo.class,"claw");
-        Intake = hmap.get(CRServo.class,"intake");
+        FRMotor = hmap.get(MotorEx.class, "FRMotor");
+        FLMotor = hmap.get(MotorEx.class, "FLMotor");
+        BRMotor = hmap.get(MotorEx.class, "BRMotor");
+        BLMotor = hmap.get(MotorEx.class, "BLMotor");
+
+        Extend = hmap.get(MotorEx.class, "Extend");
+
+        Intake = hmap.get(CRServo.class, "Intake");
+
     }
+
 
     public void loop() {
 
