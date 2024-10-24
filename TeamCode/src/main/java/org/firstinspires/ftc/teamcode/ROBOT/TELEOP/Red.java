b23273de2c5a6471ad54299fab4e156a34be4152
y33t3r3r33 <dumasdd9@gmail.com>
2024-10-23 19:55:45 -0500

    Starting red teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ROBOT/TELEOP/Red.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ROBOT/TELEOP/Red.java
@@ -1,20 +1,25 @@
 package org.firstinspires.ftc.teamcode.ROBOT.TELEOP;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.ROBOT.Hardware;
 
 @TeleOp(name="RedTeleOp")
-public class Red extends LinearOpMode {
-   private Hardware robot = Hardware.instance;
+public class Red extends OpMode {
+
 
 
     @Override
-    public void runOpMode() {
-        robot = robot.getInstance();
-        robot.init(hardwareMap);
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
 
     }
 }
+
