47ee2750cf7d778383d215f1997dae9d96435045
y33t3r3r33 <dumasdd9@gmail.com>
2024-10-28 15:33:03 -0500

    neuron activation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ROBOT/COMMANDS/DriveTrainCOMM.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ROBOT/COMMANDS/DriveTrainCOMM.java
@@ -2,5 +2,10 @@ package org.firstinspires.ftc.teamcode.ROBOT.COMMANDS;
 
 import com.arcrobotics.ftclib.command.CommandBase;
 
+import org.firstinspires.ftc.teamcode.ROBOT.SUBSYSTEMS.DrivetrainSUB;
+
 public class DriveTrainCOMM extends CommandBase {
+
+    private DrivetrainSUB dt;
+
 }
