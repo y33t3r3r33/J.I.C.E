a874b807547688d2ac9e292fecd8515e87605029
y33t3r3r33 <dumasdd9@gmail.com>
2024-10-28 13:36:27 -0500

    Listen to chromocopia by tyler the creator

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ROBOT/TELEOP/Skibidi_teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ROBOT/TELEOP/Skibidi_teleop.java
@@ -3,18 +3,15 @@ package org.firstinspires.ftc.teamcode.ROBOT.TELEOP;
 import com.arcrobotics.ftclib.drivebase.MecanumDrive;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.ROBOT.Hardware;
-import org.firstinspires.ftc.teamcode.ROBOT.Config;
-import org.firstinspires.ftc.teamcode.ROBOT.SUBSYSTEMS.DrivetrainSUB;
-import org.firstinspires.ftc.teamcode.ROBOT.SUBSYSTEMS.ArmSUB;
 
-@Disabled
+
+@TeleOp(name = "Skibidi_teleop")
 public class Skibidi_teleop extends OpMode {
     //private PIDController PIDController = new PIDController(0.1);
     private Hardware robot = Hardware.instance;
-
-    //public MecanumDrive drivesigma;
 
     @Override
     public void init() {
@@ -29,7 +26,5 @@ public class Skibidi_teleop extends OpMode {
                 gamepad1.left_stick_y * 0.8,
                 -gamepad1.right_stick_x * 0.8);
 
-
-        double power = gamepad2.left_stick_y;
     }
 }
