84e358c8c9d1238d81897e6bcbb8c46e396f68d3
y33t3r3r33 <dumasdd9@gmail.com>
2024-10-28 18:48:50 -0500

    The whip and nae nae is a hard song

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ROBOT/COMMANDS/DriveTrainCOMM.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ROBOT/COMMANDS/DriveTrainCOMM.java
@@ -4,8 +4,48 @@ import com.arcrobotics.ftclib.command.CommandBase;
 
 import org.firstinspires.ftc.teamcode.ROBOT.SUBSYSTEMS.DrivetrainSUB;
 
+import java.util.function.DoubleSupplier;
+
 public class DriveTrainCOMM extends CommandBase {
 
     private DrivetrainSUB dt;
 
+    private DoubleSupplier xSupplier;
+    private DoubleSupplier ySupplier;
+    private DoubleSupplier zSupplier;
+
+    public DriveTrainCOMM(DrivetrainSUB dt, DoubleSupplier xSupplier, DoubleSupplier ySupplier, DoubleSupplier zSupplier) {
+        this.dt = dt;
+        this.xSupplier = xSupplier;
+        this.ySupplier = ySupplier;
+        this.zSupplier = zSupplier;
+
+        addRequirements(dt);
+    }
+
+    @Override
+    public void initialize() {
+        super.initialize();
+    }
+
+    @Override
+    public void execute() {
+
+
+        this.dt.drive.driveRobotCentric(
+                -this.xSupplier.getAsDouble() * 0.8
+                , this.ySupplier.getAsDouble() * 0.8
+                , -this.zSupplier.getAsDouble() * 0.8);
+
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        super.end(interrupted);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return super.isFinished();
+    }
 }
