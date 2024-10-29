1bc7d3f5f8715a64ae98dc184e91e5c135985340
y33t3r3r33 <dumasdd9@gmail.com>
2024-10-28 19:46:48 -0500

    I am done coding for the night

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ROBOT/COMMANDS/ArmANGCOMM.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ROBOT/COMMANDS/ArmANGCOMM.java
@@ -3,4 +3,6 @@ package org.firstinspires.ftc.teamcode.ROBOT.COMMANDS;
 import com.arcrobotics.ftclib.command.CommandBase;
 
 public class ArmANGCOMM extends CommandBase {
+
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ROBOT/COMMANDS/ArmEXTCOMM.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ROBOT/COMMANDS/ArmEXTCOMM.java
@@ -2,13 +2,16 @@ package org.firstinspires.ftc.teamcode.ROBOT.COMMANDS;
 
 import com.arcrobotics.ftclib.command.CommandBase;
 
+import org.firstinspires.ftc.teamcode.ROBOT.SUBSYSTEMS.ArmSUB;
 import org.firstinspires.ftc.teamcode.ROBOT.SUBSYSTEMS.IntakeSUB;
 
 public class ArmEXTCOMM extends CommandBase {
-    public IntakeSUB intake;
+    ArmSUB arm;
 
-    public ArmEXTCOMM(IntakeSUB intake){
-        this.intake = intake;
+    public ArmEXTCOMM(ArmSUB arm) {
+        this.arm = arm;
+
+        addRequirements(this.arm);
     }
 
 
