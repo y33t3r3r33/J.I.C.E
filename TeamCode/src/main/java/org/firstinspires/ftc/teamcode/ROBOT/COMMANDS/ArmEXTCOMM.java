1758945d0ce06261feecaacc4b1a72e8ccad5ce4
y33t3r3r33 <dumasdd9@gmail.com>
2024-10-29 09:26:49 -0500

    School is fun (sometimes)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ROBOT/COMMANDS/ArmEXTCOMM.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ROBOT/COMMANDS/ArmEXTCOMM.java
@@ -3,10 +3,13 @@ package org.firstinspires.ftc.teamcode.ROBOT.COMMANDS;
 import com.arcrobotics.ftclib.command.CommandBase;
 
 import org.firstinspires.ftc.teamcode.ROBOT.SUBSYSTEMS.ArmSUB;
-import org.firstinspires.ftc.teamcode.ROBOT.SUBSYSTEMS.IntakeSUB;
+
+import java.util.function.DoubleSupplier;
 
 public class ArmEXTCOMM extends CommandBase {
     ArmSUB arm;
+
+    private DoubleSupplier ext;
 
     public ArmEXTCOMM(ArmSUB arm) {
         this.arm = arm;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/readme.md
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/readme.md
@@ -1,1 +1,7 @@
-Where my brain works
+Note to andrew for who ever sees this
+
+sigma
+skibidi
+
+you will never escape the brain rot
+you will never escape genshin impact
